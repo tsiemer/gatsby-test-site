a4a3576af571aa21f0d0c8106f4f3ea4c05cbe3a
Trinity Siemer <tsiemer@gmail.com>
2019-01-30 08:41:21 -0600

    Adding Netlify CMS

--- src/pages/index.js
+++ src/pages/index.js
@@ -46,25 +46,6 @@ export default class IndexPage extends React.Component{
               <a class="button" href="/blogs">Go!</a>
             </div>
           </div>
-          
-          <div class="flex-item"> 
-            <h1 class="classyTitle">Submit a new post</h1>
-            {/* <Form>
-              <form onSubmit={this.handleSubmit}>
-                <div class="input-container d-flex">
-                  <input name="title" value={this.state.title} onChange={this.handleInputChange} placeholder="Title"/>
-                  <input name="quote" value={this.state.quote} onChange={this.handleInputChange} placeholder="Quote"/>
-                  <input name="description" value={this.state.description} onChange={this.handleInputChange} placeholder="Description"/>
-                  <input name="content" value={this.state.content} onChange={this.handleInputChange} placeholder="Content"/>
-                  <input type="file" name="filename" value={this.state.filename} onChange={this.handleInputChange} placeholder="File Name"/>
-                </div>
-                <i>End the filename with .md please!</i>
-                <div class="button-container">
-                  <button class="button" type="Submit">Submit </button>
-                </div>
-              </form>
-            </Form> */}
-          </div>
         </Section>
   
         <Section>
